48544dd768d0d2053e988ee4fe1b2e58ecf57166
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-25 23:20:16 +0300

    olivelite: Bringup to CrDroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The radiantOS Project
+# Copyright (C) 2021 The CrDroid Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/radiant_olives.mk
+    $(LOCAL_DIR)/lineage_olives.mk
 
 COMMON_LUNCH_CHOICES := \
-    radiant_olives-user \
-    radiant_olives-userdebug \
-    radiant_olives-eng
+    lineage_olives-user \
+    lineage_olives-userdebug \
+    lineage_olives-eng
